9dbd2bb2a766c3e36648282acf561383d69f28bb
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2025-06-30 20:21:42 +0100

    added proper availabilty response type

--- src/services/mentorAvailability.ts
+++ src/services/mentorAvailability.ts
@@ -1,13 +1,19 @@
 import { prisma } from "../config/prisma.js";
 import { createAvailabilityDtoType } from "../dtos/user.dto.js";
+import { availability, Day } from "../types/user.types.js";
 
-export const createAvailability = async (mentorId: number, data: createAvailabilityDtoType) => {
+export const createAvailability = async (mentorId: number, data: createAvailabilityDtoType): Promise<availability> => {
     const availability = await prisma.availability.create({
         data: {
             mentorId,
             ...data
         },
     });
-    return availability;
+    const availabilityResponse: availability = {
+        day: availability.day as Day,
+        startTime: availability.startTime,
+        endTime: availability.endTime,
+    };
+    return availabilityResponse;
 };
 
--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -6,6 +6,16 @@ export enum Role {
   Mentee = 'Mentee',
 }
 
+export enum Day {
+  Monday = 'Monday',
+  Tuesday = 'Tuesday',
+  Wednesday = 'Wednesday',
+  Thursday = 'Thursday',
+  Friday = 'Friday',
+  Saturday = 'Saturday',
+  Sunday = 'Sunday',
+}
+
 export interface UserResponse {
   id: number;
   email: string;
@@ -26,4 +36,10 @@ export interface UserProfile {
 
 export interface fullUserProfile extends UserProfile {
   role: string
+}
+
+export interface availability {
+  day: Day;
+  startTime: string;
+  endTime: string;
 }
