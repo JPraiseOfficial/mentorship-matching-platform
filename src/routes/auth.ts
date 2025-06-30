4832786b8f0e92df7deff58a345e59ab27de45ad
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2025-06-30 19:24:10 +0100

    created role-based access middleware and applied to register endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { getUser, register } from "../controllers/auth/auth.user.js";
 import { login, logout } from "../controllers/auth/auth.js";
 import { auth } from "../middleware/auth.js";
+import { authorize } from "../middleware/authorise.js";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/register", auth, authorize("Admin"), register);
 router.post("/login", login);
 router.post("/logout", logout);
 router.get("/me", auth, getUser);
