660d88ae89c4d914e160f8fe95b0d8cd66e3d65c
JPraiseOfficial <jimmypraise.dev@gmail.com>
2025-10-27 14:13:01 +0100

    added test suites for createProfile service function

---
+++ server/src/services/users.test.ts
@@ -0,0 +1,68 @@
+import prismaMock from "../tests/mocks/prismaMock.js";
+import { makeUser, newUser } from "../tests/fixtures/users.js";
+
+jest.mock("../config/prisma", () => ({
+  prisma: prismaMock,
+}));
+
+import * as services from "./users.js";
+import { UserProfile } from "../types/types.js";
+import { createProfileDtoType } from "../dtos/dtos.js";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+const fakeProfile: UserProfile = {
+  id: 1,
+  name: "Test User",
+  bio: "Test Bio",
+  skills: ["JavaScript", "TypeScript"],
+  goals: "Test goals",
+  userId: 1,
+};
+
+const createFakeProfile: createProfileDtoType = {
+  name: "Test User",
+  bio: "Test Bio",
+  skills: ["JavaScript", "TypeScript"],
+  goals: "Test goals",
+};
+
+// createProfile Test Suites
+describe("createProfile service", () => {
+  // Check if User already has a profile
+  it("should throw error if user already has a profile", async () => {
+    prismaMock.profile.findUnique.mockResolvedValue(fakeProfile);
+
+    await expect(
+      services.createProfile(fakeProfile.userId, createFakeProfile)
+    ).rejects.toThrow("User already has a Profile");
+  });
+
+  //   Create a new profile if user has no profile
+  it("should create user profile if user has no profile", async () => {
+    prismaMock.profile.findUnique.mockResolvedValue(null);
+    prismaMock.profile.create.mockResolvedValue(fakeProfile);
+
+    const result = await services.createProfile(
+      fakeProfile.userId,
+      createFakeProfile
+    );
+
+    expect(prismaMock.profile.create).toHaveBeenCalledWith({
+      data: {
+        ...createFakeProfile,
+        user: { connect: { id: fakeProfile.userId } },
+      },
+    });
+    expect(result).toEqual({
+      id: 1,
+      name: "Test User",
+      bio: "Test Bio",
+      skills: ["JavaScript", "TypeScript"],
+      goals: "Test goals",
+      userId: 1,
+    });
+  });
+});
--- server/src/tests/mocks/prismaMock.ts
+++ server/src/tests/mocks/prismaMock.ts
@@ -1,4 +1,4 @@
-export const prismaMock: any = {
+export const prismaMock = {
   user: {
     findUnique: jest.fn(),
     create: jest.fn(),
